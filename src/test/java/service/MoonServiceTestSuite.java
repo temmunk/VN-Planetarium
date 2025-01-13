f288c3d543c4fb252f70b403c6c4c19ae0c03cce
Revature-Nafeisha <nafeisha780@revature.net>
2025-01-13 11:08:48 -0500

    Added MoonServicePositiveTests

---
+++ src/test/java/service/MoonServiceTestSuite.java
@@ -0,0 +1,12 @@
+package service;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import static org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+        MoonServicePositiveTest.class
+})
+public class MoonServiceTestSuite {
+}
